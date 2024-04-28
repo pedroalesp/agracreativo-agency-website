98506a7a0a442e17fd9d93b8436e46ffc9e7e4a2
Pedro Silva <pedro.silva@i-car.cl>
2024-04-27 20:17:13 -0400

    ✨feat/Featured Hero and Services sections

---
+++ src/consts/services.ts
@@ -0,0 +1,37 @@
+export const SERVICES = [
+  {
+    step: "01",
+    name: "Social Media",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.",
+  },
+  {
+    step: "02",
+    name: "Creación de Contenido",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.",
+  },
+  {
+    step: "03",
+    name: "Meta Ads",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.",
+  },
+  {
+    step: "04",
+    name: "Desarrollo Web",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.",
+  },
+  {
+    step: "05",
+    name: "Asesorías 1:1",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.",
+  },
+  {
+    step: "06",
+    name: "Email Marketing",
+    description: "",
+  },
+];
